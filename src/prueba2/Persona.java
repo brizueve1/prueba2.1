a3563ad404bc37cd3c22865ba031270db9b32161
L3tt1 <barboza.leticia@gmail.com>
2023-08-09 16:33:29 -0300

    Nueva clase Persona
    Cree una clase Persona con los atributos nombre y apellido, su constructor y los getter y setter.

---
+++ src/prueba2/Persona.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Persona {
+    
+    private String nombre, apellido;
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+    
+    
+}
